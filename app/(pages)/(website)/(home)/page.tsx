da865a682780b5ea1f1745004509d5947b60144f
Amhjadkhan <amjadpogramer@gmail.com>
2024-08-15 13:45:04 +0400

    this is logo added commit

--- app/(pages)/(website)/(home)/page.tsx
+++ app/(pages)/(website)/(home)/page.tsx
@@ -1,8 +1,15 @@
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main>
-      <h1>this is our app</h1>
+      <h1>this is our app home</h1>
+       <Image
+       src='/assets/images/logo.png'
+         width='200'
+         height='200'
+         alt='logo'
+       />
     </main>
   );
 }
